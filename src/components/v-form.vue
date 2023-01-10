2081bf31a1dd10b6a148b9b3c03bec2b61cdcec6
CLoudXVII <cloudofficial@bk.ru>
2023-01-10 20:42:43 +0300

    Сделал форму, реализовал переход по кнопке до неё

--- src/components/v-form.vue
+++ src/components/v-form.vue
@@ -1,13 +1,26 @@
 <template>
     <div class="vForm">
         <div class="form-text">
-            <h1>Заказать уборку</h1>
-            <p class="form-text-bottom">Свяжитесь с нами сейчас и получите 10 % скидку на все услуги </p>
+            <h1 id="form" class="form-text-top">Заказать уборку</h1>
+            <p class="form-text-bottom">Свяжитесь с нами сейчас и получите 10 % скидку на первую покупку </p>
         </div>
         <div class="form">
+            <input class="form-elem" placeholder="Введите имя" type="text" />
+            <input class="form-elem" placeholder="Введите e-mail" type="email" />
+            <input class="form-elem" placeholder="Введите номер телефона" type="tel" />
+            <select class="select-cleaning" name="Cleaning">
+                <option value="1">Обычная</option>
+                <option value="2">Генеральная</option>
+                <option value="3">Генеральная (с дезинсекцией)</option>
+            </select>
 
+            <div class="btn">
+                <a href="#"><button class="button">Оставить заявку</button></a>>
+            </div>
         </div>
-        
+        <div class="form-aftertext">
+                <h1>Наш менеджер перезвонит вам и <br>уточнит все моменты и назначит день <br>уборки</h1>
+        </div>
     </div>   
 </template>
 
@@ -31,10 +44,57 @@
 
 
 <style>
-    .form-text {
+    .vForm {
         text-align: center;
     }
+    .form-text-top {
+        font-size: 50px;
+    }
     .form-text-bottom {
-        font-weight: 500; 
+        font-weight: 500;
+        font-size: 30px; 
+    }
+    .form {
+        text-align: center;
+        background-image: url(./bgform.png);
+        background-repeat: no-repeat;
+    }
+    .form-elem {
+        width: 1194px;
+        height: 100px;
+        margin: 32px;
+        border-radius: 18px;
+        border: 1px solid #000000;
+        font-size: 36px;
+        padding-left: 30px;
+    }
+    .form-text-middle {
+        font-weight: 700;
+        font-size: 40px; 
+    }
+    .select-cleaning {
+        width: 1228px;
+        height: 100px;
+        margin: 32px;
+        border-radius: 18px;
+        border: 1px solid #000000;
+        font-size: 36px;
+        padding-left: 30px;
+    }
+    .button {
+        background-color: #FF7400;
+        color: #fff;
+        text-decoration: none;
+        padding: 24px 24px;
+        font-weight: 700;
+        transition: background-color 0.2s linear;
+        border-radius: 15px;
+        border-width: 0.5px;
+        font-size: 21px;
+        margin-bottom: 50px;
+    }
+    .form-aftertext {
+        color: black;
+        margin: 50px;
     }
 </style>
--- src/components/v-header.vue
+++ src/components/v-header.vue
@@ -33,7 +33,7 @@
                         <p>Оказываем полный спектр услуг клининга квартир <br>и помещений в Москве и области</p> 
                     </div>
                     <div class="btn">
-                        <button class="button">Оставить заявку</button>
+                        <a href="#form"><button class="button">Оставить заявку</button></a>
                     </div>
             </div>
 
@@ -88,7 +88,7 @@
         color: #fff;
         text-decoration: none;
         font-weight: 700;
-        font-size: 14px;
+        font-size: 21px;
         margin-right: 25px;
         transition: color 0.2s linear;
     }
@@ -105,6 +105,7 @@
         font-weight: 700;
         transition: color 0.2s linear;
         margin-left: 10px;
+        font-size: 21px;
     }
     .num:hover {
         color:orange;
@@ -113,18 +114,20 @@
         background-color: #FF7400;
         color: #fff;
         text-decoration: none;
-        padding: 18px 22px;
+        padding: 24px 24px;
         font-weight: 700;
         transition: background-color 0.2s linear;
         border-radius: 15px;
         border-width: 0.5px;
+        font-size: 21px;
     }
     .button:hover {
         background-color: #a8530b;
     }
     .btn-text {
         color: black;
-        font-weight: 500;        
+        font-weight: 500; 
+        font-size: 21px;       
     }
     .calltoaction {
         margin-left: 200px;
